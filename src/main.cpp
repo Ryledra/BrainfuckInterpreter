eeb8df81a02de660cd2548ed57659784140887d5
Vincent Martin <vince@ryledran.com>
2022-11-01 21:48:14 +0000

    Rearranged main() to make a guard clause of the args count

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,17 @@
 #include "loop.hpp"
 
 int main(int argc, char *argv[])  {
-
-    if (argc == 2)  {
-        std::filesystem::path path { argv[1] };
-        Loop l { readFile::file_contents(path) };
-        l.runScript();
-        std::cout << '\n';
-    } else  {
-        std::cout << "Error: Wrong number of arguments. Expected 1 file-path argument, recieved " << argc-1 << ".\n";
+    if (argc != 2)  {
+        std::cout << "Error: Wrong number of arguments."
+            << " Expected 1 file-path argument, recieved "
+            << argc-1 << ".\n";
+        return 1;
     }
+
+    std::filesystem::path path { argv[1] };
+    Loop l { readFile::file_contents(path) };
+    l.runScript();
+    std::cout << '\n';
     
     return 0;
 }
